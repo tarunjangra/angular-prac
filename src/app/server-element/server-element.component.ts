5509c68d9bc4d02585843d192b8a293c40669951
Tarun Jangra <tarun@izap.in>
2018-08-13 10:21:29 +0530

    lifecycle understanding

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  OnChanges,
+  SimpleChanges
+ } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
 
   @Input('srvElement') element: {name: string,type: string, content: string};
-  constructor() { }
+  constructor() {
+
+    console.log('Constructor of server elements');
+
+   }
 
   ngOnInit() {
+    console.log('ngOnInit call on server element.');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log(changes);
   }
 
 }
